df45b0b30ff4a4c5b1300881814c7e8390c0d7ad
LHyoo123798 <112948259+LHyoo123798@users.noreply.github.com>
2022-10-04 18:52:02 +0800

    test2

---
+++ test2/app/src/main/java/com/test/test2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.test.test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.linear_layout);
+//        setContentView(R.layout.table_layout);
+//        setContentView(R.layout.constraint_layout);
+        setContentView(R.layout.constraint_layout2);
+    }
+}
